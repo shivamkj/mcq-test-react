97386f725caf54c11a8316092b7687e432a2aae9
Shivam <info@shivamjha.com>
2025-10-31 14:00:24 +0530

    firebase project details updated

--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgxm612M00tlw7t-ENKBQ00e8lmvNYbEY",
-  authDomain: "mcqtest-8c924.firebaseapp.com",
-  projectId: "mcqtest-8c924",
-  storageBucket: "mcqtest-8c924.firebasestorage.app",
-  messagingSenderId: "367636037562",
-  appId: "1:367636037562:web:87ad36de779922c411e783",
+  apiKey: "AIzaSyCdFHy63-_Pk6egfCOb1hGBdAEPs758VVQ",
+  authDomain: "mcqtestapp-a1465.firebaseapp.com",
+  projectId: "mcqtestapp-a1465",
+  storageBucket: "mcqtestapp-a1465.firebasestorage.app",
+  messagingSenderId: "771072548401",
+  appId: "1:771072548401:web:963165f4a357a7b9d63f10"
 };
 
 const app = initializeApp(firebaseConfig);
